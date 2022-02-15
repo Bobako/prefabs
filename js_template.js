e0f174135d62cdb665c7a6a5aa50781e27149654
bobako <abqu144@gmail.com>
2022-02-15 10:30:56 +0300

    JS хуйни

---
+++ js_template.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+
+});
+// жабаскриптная хуйня для ожидания подгрузки страницы
+
+document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale='+(1/window.devicePixelRatio));
+// способ трахнуть мобилки
+
